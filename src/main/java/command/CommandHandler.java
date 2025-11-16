62bce733373480d303330c73aaefc7b92e3b7787
wireser <wireser@gmail.com>
2025-11-16 18:59:23 +0100

    Updated structure

---
+++ src/main/java/command/CommandHandler.java
@@ -0,0 +1,18 @@
+package command;
+
+import model.Profile;
+
+/**
+ * Functional interface for command execution logic.
+ */
+public interface CommandHandler {
+	/**
+	 * Executes a command.
+	 * 
+	 * @param player The player executing the command
+	 * @param command The command label used
+	 * @param args The command arguments
+	 * @throws CommandException if an error occurs during execution
+	 */
+	void execute(Profile player, String command, String[] args) throws CommandException;
+}
